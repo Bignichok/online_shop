db16f145d9ddc20ceb0ca1c52e919f3637e9318b
Bignichok <inikita546@gmai.com>
2022-01-30 15:42:36 +0200

    fixed base route path

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('./api', router);
+app.use('/api', router);
 
 const start = async () => {
     try {
